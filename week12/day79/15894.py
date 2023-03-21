8ca016fcc05acb10713d66791bf9724cacbe2430
Gyuil-Hwang <hwanggyuil@gmail.com>
2023-03-21 21:54:57 +0900

    day79

---
+++ week12/day79/15894.py
@@ -0,0 +1,9 @@
+## 15894
+import sys
+input = sys.stdin.readline
+
+def solve(n):
+    return (2*n)+(2*n)
+
+n = int(input())
+print(solve(n = n))
---
+++ week12/day79/27323.py
@@ -0,0 +1,10 @@
+## 27323
+import sys
+input = sys.stdin.readline
+
+def Square(a, b):
+    return a*b
+
+a = int(input())
+b = int(input())
+print(Square(a = a, b = b))
