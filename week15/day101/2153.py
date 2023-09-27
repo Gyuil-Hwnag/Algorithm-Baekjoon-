e4360eeaa440ed95613cd17ed60145dd22597451
Gyuil-Hwang <hwanggyuil@gmail.com>
2023-09-27 11:08:51 +0900

    day102

--- week15/day101/2153.py
+++ week15/day101/2153.py
@@ -1,24 +1,19 @@
 ## 2153
 import math
 
-# 단어 입력
 word = input()
-
-# 합 구하기
 total = 0
 for w in word:
     if w.islower():
-        total += ord(w) - 96    # a : 97
+        total += ord(w) - 96 
     else:
-        total += ord(w) - 38    # A : 65
+        total += ord(w) - 38 
 
-# 소수 판별
 isPrime = True
 for i in range(2, int(math.sqrt(total)) + 1):
     if total % i == 0:
         isPrime = False
 
-# 출력
 if isPrime:
     print('It is a prime word.')
 else:
