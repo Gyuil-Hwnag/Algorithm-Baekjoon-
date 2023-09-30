536f34c4b21b524aed2ad0dec95ddbf9a908c40d
Gyuil-Hwang <hwanggyuil@gmail.com>
2023-09-30 23:02:50 +0900

    day105

---
+++ week15/day105/1106.py
@@ -0,0 +1,14 @@
+## 1106
+input = input().split()
+
+C, N = map(int, input())
+
+cost_list = [list(map(int,input())) for _ in range(N)]
+dp = [1e7 for _ in range(C+100)]
+dp[0] = 0
+ 
+for cost, num_people in cost_list:
+    for i in range(num_people, C+100):
+        dp[i] = min(dp[i - num_people] + cost, dp[i])
+ 
+print(min(dp[C:]))
---
+++ week15/day105/5613.py
@@ -0,0 +1,12 @@
+## 5613
+res = int(input())
+while 1:
+    op = input()
+    if op == '=':
+        break
+    n = int(input())
+    if op == '+': res += n;
+    elif op == '-': res -= n;
+    elif op == '*': res *= n;
+    elif op == '/': res //= n;
+print(res)
